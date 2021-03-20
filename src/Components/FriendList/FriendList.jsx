4932451adfc21a6bae77c3b603acfcff233e3377
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-03-20 23:04:18 +0200

    fix

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -1,32 +1,22 @@
-import PropTypes from "prop-types";
-
+import styles from "./FriendList.module.css";
+import PropTypes from 'prop-types';
 import FriendListItem from "./FriendListItem";
-import styles from "./FriendList.module.css"
-
 const FriendList = ({ friends }) => (
     <ul className={styles.friendList}>
-        {friends.map(({ avatar, name, isOnline, id }) => (
-            <li key={id} className={styles.item} >
-                <FriendListItem
-                    avatar={avatar}
-                    name={name}
-                isOnline={isOnline}
+        {friends.map(({ id, avatar, name, isOnline, }) => (
+            
+            <FriendListItem
                 id={id}
+                avatar={avatar}
+                name={name}
+                isOnline={isOnline}
+                
                 />
-              </li>  
+              
         ))}
 </ul>
 )
-
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            avatar:PropTypes.string.isRequired,
-                name:PropTypes.string.isRequired,
-            isOnline:PropTypes.bool.isRequired,
-            id:PropTypes.number.isRequired,
-
-        })
-    )
-}
-export default FriendList
+    friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+  };
+export default FriendList;
--- src/Components/FriendList/FriendListItem.jsx
+++ src/Components/FriendList/FriendListItem.jsx
@@ -1,10 +1,23 @@
-import styles from "./FriendList.module.css"
-const FriendListItem = ({ avatar, name, isOnline }) => (
-    <>
-        <span className={isOnline ? styles.isOnline : styles.offLine }></span>
+import PropTypes from "prop-types";
+import styles from "./FriendList.module.css";
+
+const FriendListItem = ({ id, avatar, name, isOnline }) => (
+    <li className={styles.item} key={id} >
+        <span className={isOnline ? styles.isOnline : styles.offLine } ></span>
         <img className={avatar} src={avatar} alt="" width="48" />
         <p className={name}>{name }</p>
-    </>    
+    </li>    
 
 )
+
+FriendListItem.propTypes = {
+    
+    avatar:PropTypes.string.isRequired,
+        name:PropTypes.string.isRequired,
+    isOnline:PropTypes.bool.isRequired,
+    id:PropTypes.number.isRequired,
+
+
+
+}
 export default FriendListItem
